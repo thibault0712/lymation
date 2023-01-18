34ed6a387547828826ae310109c9142337488cf4
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-01-18 17:27:44 +0100

    Delete public_html directory

--- public_html/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=Nom_Base_De_Donnée;charset=utf8', 'Identifiant', 'MDPSecure');
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
--- public_html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    setcookie('auto_log');
-    unset($_COOKIE['auto_log']);
-    header('Location:/index.php');
-?>
